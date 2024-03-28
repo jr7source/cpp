4f1d1489ceb0eb4353d63adaf3344f6cf45146ef
Jatin Ramchandani <82888172+jr7source@users.noreply.github.com>
2024-03-28 14:51:14 +0100

    Update MedianOf2SortedArrays.cpp

--- MedianOf2SortedArrays.cpp
+++ MedianOf2SortedArrays.cpp
@@ -1,3 +1,6 @@
+// Solution to leetcode Hard problem 4. Median of Two Sorted Arrays
+// Author: Jatin 
+
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
